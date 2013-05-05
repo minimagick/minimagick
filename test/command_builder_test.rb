8b8a2cc6c8ff999121d2d420dd7192d8141476a5
Felix Bünemann <buenemann@louis.info>
2013-05-05 14:59:10 +0200

    Add strict mode for secure command execution
    This adds a new setting `MiniMagick.strict_mode` that enables some
    sanity checks for `CommandBuilder` execution.

--- test/command_builder_test.rb
+++ test/command_builder_test.rb
@@ -6,11 +6,13 @@ class CommandBuilderTest < Test::Unit::TestCase
   def setup
     @processor_path = MiniMagick.processor_path
     @processor = MiniMagick.processor
+    @strict_mode = MiniMagick.strict_mode
   end
 
   def teardown
     MiniMagick.processor_path = @processor_path
     MiniMagick.processor = @processor
+    MiniMagick.strict_mode = @strict_mode
   end
 
   def test_basic
@@ -86,5 +88,48 @@ class CommandBuilderTest < Test::Unit::TestCase
     c = CommandBuilder.new('test')
     c.auto_orient
     assert_equal c.command, "/a/strange/path/processor test -auto-orient"
+  end
+
+  def test_valid_processor_path_in_strict_mode
+    MiniMagick.strict_mode = true
+    MiniMagick.processor_path = "test"
+    c = CommandBuilder.new('identify')
+    assert_equal c.command, "test/identify"
+  end
+
+  def test_invalid_processor_path_in_strict_mode
+    MiniMagick.strict_mode = true
+    MiniMagick.processor_path = "/a/stange/path"
+    assert_raise MiniMagick::StrictModeError, "'/a/strange/path' is not a valid directory" do
+      CommandBuilder.new('identify')
+    end
+  end
+
+  def test_valid_processor_in_strict_mode
+    MiniMagick.strict_mode = true
+    MiniMagick.processor = "gm"
+    c = CommandBuilder.new('identify')
+    assert_equal c.command, "gm identify"
+  end
+
+  def test_invalid_processor_in_strict_mode
+    MiniMagick.strict_mode = true
+    MiniMagick.processor = "test"
+    assert_raise MiniMagick::StrictModeError, "processor must be either nil or 'gm'" do
+      CommandBuilder.new('identify')
+    end
+  end
+
+  def test_valid_tool_in_strict_mode
+    MiniMagick.strict_mode = true
+    c = CommandBuilder.new('identify')
+    assert_equal c.command, "identify"
+  end
+
+  def test_invalid_tool_in_strict_mode
+    MiniMagick.strict_mode = true
+    assert_raise MiniMagick::StrictModeError, "'test' is not in the list of allowed tools: convert, composite, identify, mogrify" do
+      CommandBuilder.new('test')
+    end
   end
 end
