69518f9a7854d122ab949beedae23afb5c4daa71
Kasper Grubbe <kawsper@gmail.com>
2013-05-28 10:31:53 +0200

    Remove the danish characters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,7 @@ module MiniMagickTestFiles
   test_files = File.expand_path(File.dirname(__FILE__) + "/files")
   SIMPLE_IMAGE_PATH        = test_files + "/simple.gif"
   MINUS_IMAGE_PATH         = test_files + "/simple-minus.gif"
-  SPECIAL_CHARS_IMAGE_PATH = test_files + "/special! _chars' )(-æøå.gif"
+  SPECIAL_CHARS_IMAGE_PATH = test_files + "/special! _chars' )(-.gif"
   TIFF_IMAGE_PATH          = test_files + "/leaves (spaced).tiff"
   NOT_AN_IMAGE_PATH        = test_files + "/not_an_image.php"
   GIF_WITH_JPG_EXT         = test_files + "/actually_a_gif.jpg"
