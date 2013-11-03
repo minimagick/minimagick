ad9645db5eca9abc41143906344fc6c3e26fd859
Thiago Fernandes Massa <thiagown@gmail.com>
2013-11-03 14:20:21 -0200

    Test for choose processor class method

--- spec/lib/mini_magick_spec.rb
+++ spec/lib/mini_magick_spec.rb
@@ -1,1 +1,16 @@
 require 'spec_helper'
+
+describe MiniMagick do
+  context '.choose_processor' do
+    it "identifies when mogrify exists" do
+      MiniMagick.class.any_instance.expects(:`).with('type -P mogrify').returns('/usr/bin/mogrify\n')
+      MiniMagick.choose_processor.should == nil
+    end
+
+    it "identifies when gm exists" do
+      MiniMagick.class.any_instance.expects(:`).with('type -P mogrify').returns('')
+      MiniMagick.class.any_instance.expects(:`).with('type -P gm').returns('/usr/bin/gm\n')
+      MiniMagick.choose_processor.should == 'gm'
+    end
+  end
+end
