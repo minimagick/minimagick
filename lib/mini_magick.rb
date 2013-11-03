659ea980d505af447533a6e1c1acce97fcad8bc7
Thiago Fernandes Massa <thiagown@gmail.com>
2013-11-03 14:25:37 -0200

    Changed choose_processor to rely on which instead of type -P

--- lib/mini_magick.rb
+++ lib/mini_magick.rb
@@ -18,9 +18,9 @@ module MiniMagick
     #
     # TODO: Write tests for this and figure out what platforms it supports
     def choose_processor
-      if `type -P mogrify`.size > 0
+      if `which mogrify`.size > 0
         return
-      elsif `type -P gm`.size > 0
+      elsif `which gm`.size > 0
         self.processor = "gm"
       end
     end
--- spec/lib/mini_magick_spec.rb
+++ spec/lib/mini_magick_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe MiniMagick do
   context '.choose_processor' do
     it "identifies when mogrify exists" do
-      MiniMagick.class.any_instance.expects(:`).with('type -P mogrify').returns('/usr/bin/mogrify\n')
+      MiniMagick.class.any_instance.expects(:`).with('which mogrify').returns('/usr/bin/mogrify\n')
       MiniMagick.choose_processor.should == nil
     end
 
     it "identifies when gm exists" do
-      MiniMagick.class.any_instance.expects(:`).with('type -P mogrify').returns('')
-      MiniMagick.class.any_instance.expects(:`).with('type -P gm').returns('/usr/bin/gm\n')
+      MiniMagick.class.any_instance.expects(:`).with('which mogrify').returns('')
+      MiniMagick.class.any_instance.expects(:`).with('which gm').returns('/usr/bin/gm\n')
       MiniMagick.choose_processor.should == 'gm'
     end
   end
