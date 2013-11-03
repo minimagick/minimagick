f86a7a13129cf50723bed86465d645bb66c21011
Thiago Fernandes Massa <thiagown@gmail.com>
2013-11-03 13:19:14 -0200

    Finished porting commander tests to rspec

--- spec/lib/mini_magick/command_builder_spec.rb
+++ spec/lib/mini_magick/command_builder_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 # All tests tagged as `ported` are ported from
 # testunit tests and are there for backwards compatibility
 
-MiniMagick.processor = '' #TODO: This should point to mogrify instead
+MiniMagick.processor = nil #TODO: This should point to mogrify instead
 
 describe MiniMagick::CommandBuilder do
   before(:each) do
@@ -34,6 +34,51 @@ describe MiniMagick::CommandBuilder do
       builder.alpha '1 3 4'
       builder.resize 'mome fingo'
       builder.args.join(" ").should == '-resize 30x40 -alpha 1\ 3\ 4 -resize mome\ fingo'
+    end
+
+    it "builds a command with multiple options and plus modifier" do
+      builder.distort.+ 'srt', '0.6 20'
+      builder.args.join(" ").should == '\+distort srt 0.6\ 20'
+    end
+
+    it "raises error when command is invalid" do
+      expect do
+        command = MiniMagick::CommandBuilder.new('test', 'path')
+        command.input 2
+      end.to raise_error(NoMethodError)
+    end
+
+    it "builds a dashed command" do
+      builder.auto_orient
+      builder.args.join(" ").should == '-auto-orient'
+    end
+
+    it "builds a dashed command via send" do
+      builder.send('auto-orient')
+      builder.args.join(' ').should == '-auto-orient'
+    end
+
+    it "builds a canvas command" do
+      builder.canvas 'black'
+      builder.args.join(' ').should == 'canvas:black'
+    end
+
+    it "sets a colorspace correctly" do
+      builder.set 'colorspace RGB'
+      builder.command.should == 'test -set colorspace\ RGB'
+    end
+
+    it "sets a processor path correctly" do
+      MiniMagick.processor_path = "/a/strange/path"
+      builder.auto_orient
+      builder.command.should == "/a/strange/path/test -auto-orient"
+    end
+
+    it "builds a processor path with processor" do
+      MiniMagick.processor_path = "/a/strange/path"
+      MiniMagick.processor = "processor"
+      builder.auto_orient
+      builder.command.should == "/a/strange/path/processor test -auto-orient"
     end
   end
 end
