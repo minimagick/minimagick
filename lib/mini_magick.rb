df3288bf3b9740316fca04d2d0c32c76f0744acd
Thiago Fernandes Massa <thiagown@gmail.com>
2013-11-14 15:44:47 -0200

    Added docs

--- lib/mini_magick.rb
+++ lib/mini_magick.rb
@@ -14,6 +14,12 @@ module MiniMagick
     attr_accessor :processor_path
     attr_accessor :timeout
 
+    ##
+    # Tries to detect the current processor based if any of the processors exist.
+    # Mogrify have precedence over gm by default.
+    #
+    # * *Returns* :
+    #   - the detected processor set as string
     def choose_processor
       if MiniMagick::Utilities.which('mogrify').size > 0
         self.processor = 'mogrify'
@@ -21,27 +27,50 @@ module MiniMagick
         self.processor = "gm"
       end
     end
-
+    
+    ##
+    # Discovers the imagemagick version based on mogrify's output.
+    #
+    # * *Returns* :
+    #   - the imagemagick version
     def image_magick_version
       @@version ||= Gem::Version.create(`mogrify --version`.split(" ")[2].split("-").first)
     end
-
+    
+    ##
+    # The minimum allowed imagemagick version
+    #
+    # * *Returns* :
+    #   - the imagemagick version
     def minimum_image_magick_version
       @@minimum_version ||= Gem::Version.create("6.6.3")
     end
 
+    ##
+    # Checks whether the imagemagick's version is valid
+    #
+    # * *Returns* :
+    #   - a boolean value
     def valid_version_installed?
       image_magick_version >= minimum_image_magick_version
     end
 
-    # Picks the right processor if it isn't set and tells if it's mogrify
+    ##
+    # Picks the right processor if it isn't set and returns whether it's mogrify or not.
+    #
+    # * *Returns* :
+    #   - a boolean value
     def mogrify?
       self.choose_processor if self.processor.nil?
 
       self.processor == 'mogrify'
     end
 
-    # Picks the right processor if it isn't set and tells if it's gm
+    ##
+    # Picks the right processor if it isn't set and returns whether it's graphicsmagick or not.
+    #
+    # * *Returns* :
+    #   - a boolean value
     def gm?
       self.choose_processor if self.processor.nil?
 
