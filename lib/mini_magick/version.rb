dc34c25ace7220534a2b3928f1ceeb0e3b0a44e8
Tobias Bohwalli <hi@futhr.io>
2014-04-20 03:38:31 +0200

    Returns the version as a Gem::Version

--- lib/mini_magick/version.rb
+++ lib/mini_magick/version.rb
@@ -1,3 +1,17 @@
 module MiniMagick
-  VERSION = '3.8.0'
+  ##
+  # Returns the version of the currently loaded MiniMagick as
+  # a <tt>Gem::Version</tt>.
+  def self.version
+    Gem::Version.new VERSION::STRING
+  end
+
+  module VERSION
+    MAJOR = 3
+    MINOR = 8
+    TINY  = 0
+    PRE   = nil
+
+    STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+  end
 end
