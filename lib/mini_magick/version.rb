2dcf37622f4131e99946f6f981c952f357e6a9cb
Janko Marohnić <janko.marohnic@gmail.com>
2015-09-02 15:26:48 +0200

    Bump to 4.3.0

--- lib/mini_magick/version.rb
+++ lib/mini_magick/version.rb
@@ -8,8 +8,8 @@ module MiniMagick
 
   module VERSION
     MAJOR = 4
-    MINOR = 2
-    TINY  = 10
+    MINOR = 3
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
