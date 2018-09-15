0c26c669cc9de8ba94067282cfc7eca76b04f406
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2018-09-15 12:59:41 +0900

    Specify and update IM 6 version

--- install/imagemagick.sh
+++ install/imagemagick.sh
@@ -1,14 +1,7 @@
 set -e
 
-case "$IM_VERSION" in
-  6)
-    sudo apt-get install imagemagick libmagickcore-dev libmagickwand-dev
-    ;;
-  7)
-    curl -O "http://www.imagemagick.org/download/ImageMagick.tar.gz"
-    tar xzf "ImageMagick.tar.gz"
-    cd ImageMagick-7*
-    ./configure --prefix=/usr
-    sudo make install
-    ;;
-esac
+curl -O "http://www.imagemagick.org/download/ImageMagick-$IM_VERSION.tar.gz"
+tar xzf "ImageMagick-$IM_VERSION.tar.gz"
+cd ImageMagick-*
+./configure --prefix=/usr
+sudo make install
