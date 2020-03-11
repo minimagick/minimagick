8ce421a060956eee4ad44322b18717b43ac99af6
Yuya.Nishida <62389+nishidayuya@users.noreply.github.com>
2020-03-12 04:28:53 +0900

    Fix Imagemagick download URI on CI (#506)
    * use xz archive instead of gzip
    
    http://www.imagemagick.org/download/releases/ImageMagick-6.9.10-33.tar.gz is 404 not found
    
    * use HTTPS for downloading

--- install/imagemagick.sh
+++ install/imagemagick.sh
@@ -1,8 +1,8 @@
 set -ex
 
-im_download_path=$(curl -sf http://www.imagemagick.org/download/releases/ | grep -o "ImageMagick-$IM_VERSION-.*.tar.gz" -m 1)
-curl -f "http://www.imagemagick.org/download/releases/$im_download_path" > ImageMagick.tar.gz
-tar xzf ImageMagick.tar.gz
+im_download_path=$(curl -sf https://www.imagemagick.org/download/releases/ | grep -o "ImageMagick-$IM_VERSION-.*.tar.xz" -m 1)
+curl -f "https://www.imagemagick.org/download/releases/$im_download_path" > ImageMagick.tar.xz
+tar xf ImageMagick.tar.xz
 cd ImageMagick-*
 ./configure --prefix=/usr
 sudo make install
