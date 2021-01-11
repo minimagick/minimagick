6a232e174fef0b67e35144ab87785cce1e190969
Masayoshi Takahashi <takahashim@users.noreply.github.com>
2021-01-12 02:03:56 +0900

    Fix script of downloading ImageMagick (#524)
    * Fix URL of downloading ImageMagick
    
    * Fix commands to donwload in install/imagemagick.sh

--- install/imagemagick.sh
+++ install/imagemagick.sh
@@ -1,7 +1,7 @@
 set -ex
 
-im_download_path=$(curl -sf https://www.imagemagick.org/download/releases/ | grep -o "ImageMagick-$IM_VERSION-.*.tar.xz" -m 1)
-curl -f "https://www.imagemagick.org/download/releases/$im_download_path" > ImageMagick.tar.xz
+im_download_path=$(curl -sf https://download.imagemagick.org/ImageMagick/download/releases/ | grep -o "ImageMagick-$IM_VERSION-.[0-9]*.tar.xz" -m 1 | head -1)
+curl -f "https://download.imagemagick.org/ImageMagick/download/releases/$im_download_path" > ImageMagick.tar.xz
 tar xf ImageMagick.tar.xz
 cd ImageMagick-*
 ./configure --prefix=/usr
