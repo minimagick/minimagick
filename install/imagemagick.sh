4bd3b900941722a04b7ebc4097bf14038e811051
Janko Marohnić <janko.marohnic@gmail.com>
2021-09-17 20:24:54 +0200

    Move from Travis CI to GitHub CI

--- install/imagemagick.sh
+++
@@ -1,8 +0,0 @@
-set -ex
-
-im_download_path=$(curl -sf https://download.imagemagick.org/ImageMagick/download/releases/ | grep -o "ImageMagick-$IM_VERSION-.[0-9]*.tar.xz" -m 1 | head -1)
-curl -f "https://download.imagemagick.org/ImageMagick/download/releases/$im_download_path" > ImageMagick.tar.xz
-tar xf ImageMagick.tar.xz
-cd ImageMagick-*
-./configure --prefix=/usr
-sudo make install
